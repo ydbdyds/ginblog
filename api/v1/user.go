9c27534ae450815a384b6abd1e4cccb51c8cc5f3
CS Chen <1004903113@qq.com>
2022-03-29 15:38:40 +0800

    完善用户接口

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,22 +1,60 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/model"
+	"ginblog/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
-//查询用户是否存在
+var code int
+
+//查询用户是否存在(不允许相同用户名)
 func UserExist(c *gin.Context) {
 
 }
 
 //添加用户
 func AddUser(c *gin.Context) {
-	//todo 添加用户
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		code = errmsg.ERROR_USERNAME_USED
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //查询单个用户
 
 //查询用户列表
 func GetUsers(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pagesize")) //query返回string转换成int
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
 
+	if pageSize == 0 { //相当于不要这个分页功能 gorm提供了一个方法 如果给limit传-1就不做限制
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = -1
+	}
+
+	data := model.GetUsers(pageSize, pageNum)
+	code = errmsg.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //编辑用户
--- model/User.go
+++ model/User.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"ginblog/utils/errmsg"
+	"github.com/jinzhu/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -8,3 +11,39 @@ type User struct {
 	Password string `gorm:"type:varchar(20);not null" json:"password"` //密码
 	Role     int    `gorm:"type:int" json:"role"`                      //权限
 }
+
+//查询用户是否存在
+func CheckUser(name string) (code int) {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+	if users.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCESS
+}
+
+//新增用户
+func CreateUser(data *User) int {
+	err := db.Create(&data).Error //入参是接口 返回是db模型所以用错误处理来接受
+	if err != nil {
+		return errmsg.ERROR //500
+	}
+	return errmsg.SUCCESS
+}
+
+//查询用户列表 分页处理 要不然一次很多容易卡顿
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err = db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return users
+}
+
+////编辑用户
+//func EditUser(id int)(code int)  {
+//
+//}
+//
+////删除用户

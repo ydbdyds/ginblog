79e43c60b591b7fa91e5abee2501d89fc9abc136
CS Chen <1004903113@qq.com>
2022-04-08 17:21:37 +0800

    fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,10 +12,10 @@ func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowAllOrigins: true, //跨域域名 允许所有的跨域
 		//AllowOrigins: []string{"*"},这个与上面的等效
-		AllowMethods:  []string{"*"}, //允许的请求方法 全部允许
-		AllowHeaders:  []string{"Origin"},
-		ExposeHeaders: []string{"Content-Length", "Authorization"},
-		//AllowCredentials: true, //是否发送cookie请求
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, //允许的请求方法 全部允许
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+		AllowCredentials: true, //是否发送cookie请求
 
 		MaxAge: 12 * time.Hour,
 	})

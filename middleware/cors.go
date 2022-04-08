50d6e7ce7d611ed317eda2e849c9fcb1e38d3bac
CS Chen <1004903113@qq.com>
2022-04-08 16:55:29 +0800

    fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,16 +8,16 @@ import (
 
 //跨域配置
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		cors.New(cors.Config{
-			AllowAllOrigins: true, //跨域域名 允许所有的跨域
-			//AllowOrigins: []string{"*"},这个与上面的等效
-			AllowMethods:  []string{"*"}, //允许的请求方法 全部允许
-			AllowHeaders:  []string{"Origin"},
-			ExposeHeaders: []string{"Content-Length", "Authorization"},
-			//AllowCredentials: true, //是否发送cookie请求
 
-			MaxAge: 12 * time.Hour,
-		})
-	}
+	return cors.New(cors.Config{
+		AllowAllOrigins: true, //跨域域名 允许所有的跨域
+		//AllowOrigins: []string{"*"},这个与上面的等效
+		AllowMethods:  []string{"*"}, //允许的请求方法 全部允许
+		AllowHeaders:  []string{"Origin"},
+		ExposeHeaders: []string{"Content-Length", "Authorization"},
+		//AllowCredentials: true, //是否发送cookie请求
+
+		MaxAge: 12 * time.Hour,
+	})
+
 }
